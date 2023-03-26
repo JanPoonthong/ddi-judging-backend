f07dbe4d2b634a9231579a035d09b9ce8278d842
Jan Poonthong <janpoonthong628@gmail.com>
2023-03-26 14:32:53 +0700

    fix: cors issue on production

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-};
 
-module.exports = nextConfig;
+// const nextConfig = {
+//     reactStrictMode: true,
+// };
+
+module.exports = {
+    async rewrites() {
+        return [
+          {
+            source: '/api/:path*',
+            destination: 'https://ddi-judging-backend.vercel.app//:path*',
+          },
+        ]
+      },
+  };
+
+// module.exports = nextConfig;
