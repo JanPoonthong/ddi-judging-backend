f1e2d6a0f1508d8c83f4831ff97b878280dff99f
Jan Poonthong <janpoonthong628@gmail.com>
2023-03-27 12:54:41 +0700

    add: function for getting all the teams

--- pages/api/getAllTeam.js
+++ pages/api/getAllTeam.js
@@ -9,14 +9,26 @@ export default async function handle(req, res) {
         optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
     });
 
+    console.log("HELLO", req.body)
+
     try {
-        let teams = await prisma.team.findMany({
-            orderBy: {
-                amount: "desc",
+        const judge = await prisma.judge.findUnique({
+            where: {
+                id: req.body.id
+            },
+            include: {
+                teamList: true,
             },
         });
-        res.json({ team: teams });
+
+        // let teams = await prisma.judge.findMany({
+        //     orderBy: {
+        //         teamsList: "desc",
+        //     },
+        // });
+        res.json({ team: judge});
     } catch (error) {
+        console.log(error)
         res.json({ error: error });
     }
 }
