8829af30f6f048480cbd9e8a2d871d57d6e192eb
Jan Poonthong <janpoonthong628@gmail.com>
2023-03-27 12:55:25 +0700

    feat: add username in api for judge names

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,5 +1,6 @@
 import { judgeList } from "./judgeList";
 import NextCors from "nextjs-cors";
+import prisma from "../../lib/prisma";
 
 export default async function login(req, res) {
     await NextCors(req, res, {
@@ -9,19 +10,42 @@ export default async function login(req, res) {
         optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
     });
 
-    let name = req.body.username;
+    let id = req.body.id;
     let password = req.body.password;
-    const judge = judgeList.find(function (element) {
-        return element.username === name && element.password === password;
-    });
+    let username = req.body.username;
 
-    if (judge) {
-        res.json({
-            success: true,
-            message: "Authenticate Success",
-            judge: judge,
+    let judge;
+
+    await prisma.judge.findUnique({
+            where: { loginID: id},
+            include: {
+                teamList: true,
+            },
         });
+    
+    try {
+        judge = await prisma.judge.update({
+            where: { loginID: id },
+            data: {
+                name: username
+            }
+        })
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, error: "No id found on database" });
+    }
+    
+    if (judge) {
+        if (judge.loginID === id && judge.password === password) {
+            res.json({
+                success: true,
+                message: "Authenticate Success",
+                judge: judge,
+            });
+        } else {
+            res.json({ success: false, error: "Wrong credential" });
+        }
     } else {
-        res.json({ success: false, error: "Wrong credential" });
+        res.json({ success: false, error: "No id found on database" });
     }
 }
