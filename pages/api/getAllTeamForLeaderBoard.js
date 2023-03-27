29f179aefb822e9d0dec5c2b5ff0eae9702a97cf
Jan Poonthong <janpoonthong628@gmail.com>
2023-03-27 12:55:02 +0700

    feat: api for getting all the team in leader board

---
+++ pages/api/getAllTeamForLeaderBoard.js
@@ -0,0 +1,47 @@
+import prisma from "../../lib/prisma";
+import NextCors from "nextjs-cors";
+import plusTotalAmount from "@/lib/plusTotalAmount";
+
+export default async function handle(req, res) {
+    await NextCors(req, res, {
+        methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+        origin: "*",
+        optionsSuccessStatus: 200,
+    });
+
+    console.log("HELLO", req.body)
+
+    try {
+         const judge = await prisma.judge.findMany({
+            include: {
+                teamList: true,
+            },
+        });
+
+        const teamTotals = {};
+
+        for (const teamDict of judge) {
+            for (const team of teamDict.teamList) {
+                const teamName = team.teamName;
+                const investmentAmount = team.investmentAmount;
+                if (!(teamName in teamTotals)) {
+                    teamTotals[teamName] = investmentAmount;
+                } else {
+                    teamTotals[teamName] += investmentAmount;
+                }
+            }
+        }
+
+        const transformedArray = Object.keys(teamTotals).map((key) => {
+            return {
+                name: key,
+                amount: teamTotals[key]
+            };
+        });
+
+        res.json({ team: transformedArray});
+    } catch (error) {
+        console.log(error)
+        res.json({ error: error });
+    }
+}
