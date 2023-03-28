1e2270a90ca8b91dd66a7b7aaa9ecd858acd914d
Jan Poonthong <janpoonthong628@gmail.com>
2023-03-28 15:38:33 +0700

    feat: api for getting history and number of transaction

---
+++ pages/api/getHistory.js
@@ -0,0 +1,19 @@
+import prisma from "../../lib/prisma";
+import NextCors from "nextjs-cors";
+
+export default async function handle(req, res) {
+    await NextCors(req, res, {
+        methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+        origin: "*",
+        optionsSuccessStatus: 200,
+    });
+
+    try {
+        const history = await prisma.history.findMany({
+        });
+        res.send({success: true, message: history})
+    } catch (error) {
+        return res.send({ success: false, error: error})
+    }
+
+}
